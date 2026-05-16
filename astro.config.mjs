22634521c759555674fd9ef18c63ebab9473386b
Saurabh Singla <saurabh.singla+PRTH@prth.com>
2026-05-17 00:17:50 +0530

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  output: 'static',
+  // TODO: Set your actual domain once you have one
+  // site: 'https://nebrix.in',
+});
